96690673080f502eabb285977d5bcc6e8d7e0bdb
Ebrahim Azarisooreh <ebaz7868@colorado.edu>
2025-11-09 01:25:53 +0000

    Implementing db_create route.

--- app.py
+++ app.py
@@ -12,6 +12,29 @@ def hello_world():
 
 @app.route('/db_test')
 def db_test():
-    connection_url = 'postgresql://eazar001_render_db_user:F6rRZMJ8EfDvOo5nSXP5aEW8PYVZ6LNh@dpg-d47u8rhr0fns73fmtb10-a/eazar001_render_db'
-    with psycopg2.connect(connection_url) as conn:
+    with connect_db_service() as conn:
         return 'Database Connection Successful'
+
+
+@app.route('/db_create')
+def db_create():
+    with connect_db_service() as conn:
+        cur = conn.cursor()
+        cur.execute(
+        '''
+            CREATE TABLE IF NOT EXISTS Basketball(
+                First varchar(255),
+                Last varchar(255),
+                City varchar(255),
+                Name varchar(255),
+                Number int
+                );
+        ''')
+        conn.commit()
+        return 'Basketball Table Successfully Created'
+
+
+def connect_db_service():
+    connection_url = 'postgresql://eazar001_render_db_user:F6rRZMJ8EfDvOo5nSXP5aEW8PYVZ6LNh@dpg-d47u8rhr0fns73fmtb10-a/eazar001_render_db'
+    conn = psycopg2.connect(connection_url)
+    return conn
